43b7e9fbd3acd4224744e63fbad574142d57bb65
Gandre1 <gexonrodriguez1822@gmail.com>
2025-02-03 16:23:34 -0500

    ajuste

--- routes/lapidaRoutes.js
+++ routes/lapidaRoutes.js
@@ -4,7 +4,7 @@ const LapidaController = require('../controllers/lapidaController');
 const router = express.Router();
 
 router.post('/', LapidaController.create); 
-router.get('/', LapidaController.getAll);
+router.get('/:id', LapidaController.getAll);
 router.delete('/:id', LapidaController.delete);
 router.get('/diseno', LapidaController.getDisenos);
 router.post('/diseno', LapidaController.createDisenoLapida); 
