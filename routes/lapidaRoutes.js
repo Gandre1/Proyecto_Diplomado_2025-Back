79b9ce9218ea7c0ab4ab63ede12e3cd5e5d58ac7
Gandre1 <gexonrodriguez1822@gmail.com>
2025-02-05 14:39:54 -0500

    first commit

--- routes/lapidaRoutes.js
+++ routes/lapidaRoutes.js
@@ -4,7 +4,8 @@ const LapidaController = require('../controllers/lapidaController');
 const router = express.Router();
 
 router.post('/', LapidaController.create); 
-router.get('/:id', LapidaController.getAll);
+router.get('/', LapidaController.getAll); 
+router.get('/:id', LapidaController.getById);
 router.delete('/:id', LapidaController.delete);
 router.get('/diseno', LapidaController.getDisenos);
 router.post('/diseno', LapidaController.createDisenoLapida); 
